452121cc4aebe99e6d3918fcd15303f63bbd4fbf
sodikin <sodikinsadani@gmail.com>
2017-11-03 17:46:51 +0700

    Configuring the app

--- crazy_dog/settings.py
+++ crazy_dog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://creazydog.herokuapp.com/','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['creazydog.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
